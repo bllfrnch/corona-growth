12f5d13a517d7334b831b697ac8e9052b4f0dadb
Bill French <bill.french@nytimes.com>
2020-05-23 18:48:46 -0400

    Added call to API, plus some baseline map functionality

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -10,6 +10,7 @@
   </div>
 </template>
 <script>
+import axios from 'axios';
 import Mapbox from 'mapbox-gl';
 import { MglMap } from 'vue-mapbox';
 import { ACCESS_TOKEN, MAP_STYLE } from '../shared/constants';
@@ -17,8 +18,8 @@ export default {
   data() {
     return {
       accessToken: ACCESS_TOKEN,
-      map: undefined,
-      center: [-98, 39],
+      cases: null,
+      center: [-98, 38.88],
       mapStyle: MAP_STYLE,
       zoom: 4,
     };
@@ -26,8 +27,58 @@ export default {
   methods: {
     onMapLoaded(ev) {
       const { map } = ev;
-      this.map = map;
+      console.log(map, map.style);
+      map.addSource('states', {
+        type: 'vector',
+        url: 'mapbox://mapbox.us_census_states_2015',
+      });
+
+      map.addSource('counties', {
+        type: 'vector',
+        url: 'mapbox://mapbox.82pkq93d',
+      });
+
+      // STATES
+      // map.addLayer(
+      //   {
+      //     id: 'states-join',
+      //     type: 'fill',
+      //     source: 'states',
+      //     'source-layer': 'states',
+      //     paint: {
+      //       'fill-color': 'rgba(50, 50, 50, 0.5)',
+      //     },
+      //   },
+      //   'waterway-label'
+      // );
+
+      // COUNTIES
+      map.addLayer(
+        {
+          id: 'counties',
+          type: 'fill',
+          source: 'counties',
+          'source-layer': 'original',
+          paint: {
+            'fill-outline-color': 'rgba(0,0,0,0.1)',
+            'fill-color': 'rgba(0,0,0,0.1)',
+          },
+        },
+        'waterway-label'
+      ); // Place polygon under these labels.
     },
+  },
+  mounted() {
+    const d = new Date();
+    const day = d.getDate() - 1; // TODO: pass max date for which we have data instead of using today
+    const month = d.getMonth() + 1; // returns 0 - 11; so helpful
+    const year = d.getFullYear();
+    const date = `${year}-${month}-${day}`;
+
+    axios.get(`/api/cases/date/${date}/US`).then((response) => {
+      console.log(response, response.data);
+      this.cases = response.data;
+    });
   },
   components: {
     MglMap,
